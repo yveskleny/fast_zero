40a0b661d04b2c46fb7a39ba91420db6312a97ec
Yves Kleny <yves.kleny.1@gmail.com>
2024-11-10 10:26:10 -0300

    Revisando configurações

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,9 +6,9 @@ from fast_zero.app import app
 
 
 def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
+    client = TestClient(app) # Arrange (organização)
 
-    response = client.get('/')
+    response = client.get('/') # Act (ação)
 
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Olá Mundo!'}
+    assert response.status_code == HTTPStatus.OK # Assert
+    assert response.json() == {'message': 'Olá Mundo!'} # Assert
