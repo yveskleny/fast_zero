93e312453b9655b9bb41cb108344fa6c60114835
Yves Kleny <yves.kleny.1@gmail.com>
2024-12-16 21:46:43 -0300

    Autenticação e Autorização com tokens JWT | Aula 06

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,7 @@ from sqlalchemy.pool import StaticPool
 from fast_zero.app import app
 from fast_zero.database import get_session
 from fast_zero.models import User, table_registry
+from fast_zero.security import get_password_hash
 
 
 @pytest.fixture
@@ -62,10 +63,26 @@ def mock_db_time():
 
 @pytest.fixture
 def user(session):
-    user = User(username='Teste', email='teste@test.com', password='testtest')
+    pwd = 'testtest'
+    user = User(
+        username='Teste',
+        email='test@test.com',
+        password=get_password_hash(pwd),
+    )
 
     session.add(user)
     session.commit()
     session.refresh(user)
 
+    user.clean_password = pwd  # Monkey Patch
+
     return user
+
+
+@pytest.fixture
+def token(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+    return response.json()['access_token']
--- tests/test_app.py
+++ tests/test_app.py
@@ -90,14 +90,15 @@ def test_read_invalid_user(client):
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_update_users(client, user):
+def test_update_users(client, user, token):
     response = client.put(
-        '/users/1',
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
         json={
             'password': 'secret',
             'username': 'chapeleiroMaluco',
             'email': 'alice@example.com',
-            'id': 1,
+            'id': user.id,
         },
     )
 
@@ -105,33 +106,48 @@ def test_update_users(client, user):
     assert response.json() == {
         'username': 'chapeleiroMaluco',
         'email': 'alice@example.com',
-        'id': 1,
+        'id': user.id,
     }
 
 
-def test_delete_user(client, user):
-    response = client.delete('/users/1')
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
 
     assert response.json() == {'message': 'User deleted'}
 
 
-def test_update_invalid_user(client):
-    response = client.put(
-        '/users/666',
-        json={
-            'password': 'secret',
-            'username': 'chapeleiroMaluco',
-            'email': 'alice@example.com',
-            'id': -1,
-        },
+# def test_update_invalid_user(client):
+#     response = client.put(
+#         '/users/666',
+#         json={
+#             'password': 'secret',
+#             'username': 'chapeleiroMaluco',
+#             'email': 'alice@example.com',
+#             'id': -1,
+#         },
+#     )
+
+#     assert response.status_code == HTTPStatus.NOT_FOUND
+#     assert response.json() == {'detail': 'User not found'}
+
+
+# def test_delete_should_return_not_found(client):
+#     response = client.delete('/users/666')
+
+#     assert response.status_code == HTTPStatus.NOT_FOUND
+#     assert response.json() == {'detail': 'User not found'}
+
+
+def test_get_token(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
     )
+    token = response.json()
 
-    assert response.status_code == HTTPStatus.NOT_FOUND
-    assert response.json() == {'detail': 'User not found'}
-
-
-def test_delete_should_return_not_found(client):
-    response = client.delete('/users/666')
-
-    assert response.status_code == HTTPStatus.NOT_FOUND
-    assert response.json() == {'detail': 'User not found'}
+    assert response.status_code == HTTPStatus.OK
+    assert 'access_token' in token
+    assert 'token_type' in token
